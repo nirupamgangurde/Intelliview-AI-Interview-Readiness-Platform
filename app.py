a2522df9fa25d76e4bf4e1e7b611ff12a50c1e83
nirupamgangurde <nirupamgangurde242@gmail.com>
2026-01-01 21:01:52 +0530

    created dashboard UI

--- app.py
+++ app.py
@@ -22,5 +22,18 @@ def signin():
         return render_template("welcome.html", name= username)
     else:
         return render_template("login.html")
+    
+@app.route("/interview/text")
+def text_interview():
+    return render_template('text.html')
+
+@app.route("/interview/audio")
+def audio_interview():
+    return render_template('audio.html')
+
+@app.route('/interview/video.html')
+def video_interview():
+    return render_template('video.html')
 if __name__ == '__main__':
     app.run(debug=True)
+
