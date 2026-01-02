07e798c7dfaf1daa0c9a238aa5f06704302db910
nirupamgangurde <nirupamgangurde242@gmail.com>
2026-01-02 16:23:21 +0530

    Designed audio.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,14 +15,25 @@ def signin():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    valid_users={
-        'Nirupam':'123'
+    # In a real app, use a database or environment variables!
+    valid_users = {
+        'Nirupam': '123'
     }
+    
     if username in valid_users and password == valid_users[username]:
-        return render_template("welcome.html", name= username)
+        # Successfully logged in
+        return render_template("welcome.html", name=username)
     else:
         return render_template("login.html")
-    
+
+# --- NEW ROUTE ADDED ---
+# This is required because text.html has a "Back" button linking to 'dashboard'
+@app.route("/dashboard")
+def dashboard():
+    # In a real app, you'd get the name from a session. 
+    # For now, we default to "User" or "Nirupam" so the page doesn't break.
+    return render_template("welcome.html", name="Nirupam")
+
 @app.route("/interview/text")
 def text_interview():
     return render_template('text.html')
@@ -31,9 +42,10 @@ def text_interview():
 def audio_interview():
     return render_template('audio.html')
 
-@app.route('/interview/video.html')
+# Fixed the URL to be consistent (removed .html)
+@app.route('/interview/video')
 def video_interview():
     return render_template('video.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
